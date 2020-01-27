344805534b928cebecde5dd6e00c61c41d8045fa
Mark Zeller <mark@nullsinn.net>
2020-01-27 21:27:44 +0100

    New commit message

--- app/settings.py
+++ app/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django_restframework',
+#    'django_restframework',
     'feeds_and_speeds',
 ]
 
